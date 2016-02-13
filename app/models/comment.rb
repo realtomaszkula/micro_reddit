772615ef16aef6bec1b31833938be36df9bace9c
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-13 19:06:12 +0100

    Add validations and first comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+
+  validates :user, presence: true
+  validates :post, presence: true
+  validates :body, presence: true, length: { in: 3..100 }
+
 end
